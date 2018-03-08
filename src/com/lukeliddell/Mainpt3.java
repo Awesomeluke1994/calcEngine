f3a26489732aa4250aa5393904b9c80b9817e787
lukeliddell94@gmail.com <Zapdos123>
2018-03-08 12:45:56 +0000

    A closer look at parameters

--- src/com/lukeliddell/Mainpt3.java
+++ src/com/lukeliddell/Mainpt3.java
@@ -4,19 +4,43 @@ public class Mainpt3 {
     public static void main(String[] args) {
 
 
-
         MathEquation[] equations = new MathEquation[4];
         equations[0] = new MathEquation('d', 100.0d, 50.0d);
         equations[1] = new MathEquation('a', 25.0d, 92.0d);
         equations[2] = new MathEquation('s', 225.0d, 17.0d);
         equations[3] = new MathEquation('m', 11.0d, 3.0d);
 
-        for(MathEquation equation : equations) {
+        for (MathEquation equation : equations) {
             equation.execute();
             System.out.print("Result = ");
             System.out.println(equation.getResult());
         }
-    }
+
+        System.out.println();
+        System.out.println("Using Overloads");
+        System.out.println();
+
+        double leftDouble = 9.0d;
+        double rightDouble = 4.0d;
+        int leftInt = 9;
+        int rightInt = 4;
+
+        MathEquation equationOverload = new MathEquation('d');
+
+        equationOverload.execute(leftDouble,rightDouble);
+        System.out.println(equationOverload.getResult());
+
+        //Widening conversion - can convert ints to doubles
+        equationOverload.execute(rightInt,rightInt);
+        System.out.print("results=");
+        System.out.println(equationOverload.getResult());
+
+        //Becuase the double is a narrow conversion into a int - it will choose the one that accepts two doubles.
+        equationOverload.execute((double)rightInt,rightInt);
+        System.out.print("results=");
+        System.out.println(equationOverload.getResult());
+
+
 
 //    public static MathEquation create(double leftVal, double rightVal, char opCode) {
 //        MathEquation equation = new MathEquation();
@@ -26,4 +50,5 @@ public class Mainpt3 {
 //
 //        return equation;
 //    }
+    }
 }
--- src/com/lukeliddell/MathEquation.java
+++ src/com/lukeliddell/MathEquation.java
@@ -8,13 +8,13 @@ public class MathEquation {
     private char opCode;
     private double result;
 
-//    public double getLeftVal(){return leftVal;}
-//    public void setLeftVal(double leftVal) {this.leftVal = leftVal;}
-//    public double getRightVal(){return rightVal;}
-//    public void setRightVal(double rightVal) {this.rightVal = rightVal;}
-//    public char getOpCode() {return opCode;}
-//    public void setOpCode(char opCode){this.opCode = opCode;}
-//    public double getResult(){return result;}
+    public double getLeftVal(){return leftVal;}
+    public void setLeftVal(double leftVal) {this.leftVal = leftVal;}
+    public double getRightVal(){return rightVal;}
+    public void setRightVal(double rightVal) {this.rightVal = rightVal;}
+    public char getOpCode() {return opCode;}
+    public void setOpCode(char opCode){this.opCode = opCode;}
+    public double getResult(){return result;}
 
     public MathEquation(){}
 
@@ -26,6 +26,21 @@ public class MathEquation {
         this(opCode);
         this.leftVal = leftVal;
         this.rightVal = rightVal;
+    }
+
+    public void execute(double leftVal, double rightVal){
+        this.leftVal = leftVal;
+        this.rightVal = rightVal;
+        execute();
+    }
+
+    public void exectue(int leftVal, int rightVal){
+        this.leftVal = leftVal;
+        this.rightVal = rightVal;
+
+        execute();
+
+        result = (int)result;
     }
 
     public void execute() {
